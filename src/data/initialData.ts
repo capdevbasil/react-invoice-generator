d8ecdce2a60ceb9bc095f31c2e50b29c8b9e9862
Basil Reji <basilreji3@gmail.com>
2024-10-30 22:41:07 +0530

    initial commit

---
+++ src/data/initialData.ts
@@ -0,0 +1,45 @@
+import { ProductLine, Invoice } from './types'
+import logo from '../assets/logo.png'
+export const initialProductLine: ProductLine = {
+  description: '',
+  quantity: '1',
+  rate: '0.00',
+}
+
+export const initialInvoice: Invoice = {
+  logo: logo,
+  logoWidth: 100,
+  title: 'INVOICE',
+  companyName: 'Weavers Project',
+  name: '',
+  companyAddress: '',
+  companyAddress2: '',
+  companyCountry: 'United States',
+  billTo: 'Bill To:',
+  clientName: '',
+  clientAddress: '',
+  clientAddress2: '',
+  clientCountry: 'United States',
+  invoiceTitleLabel: 'Invoice#',
+  invoiceTitle: '',
+  invoiceDateLabel: 'Invoice Date',
+  invoiceDate: '',
+  invoiceDueDateLabel: 'Due Date',
+  invoiceDueDate: '',
+  productLineDescription: 'Item Description',
+  productLineQuantity: 'Qty',
+  productLineQuantityRate: 'Rate',
+  productLineQuantityAmount: 'Amount',
+  productLines: [
+    { ...initialProductLine },
+    { ...initialProductLine },
+  ],
+  subTotalLabel: 'Sub Total',
+  taxLabel: 'Sale Tax (10%)',
+  totalLabel: 'TOTAL',
+  currency: 'Rs.',
+  notesLabel: 'Notes',
+  notes: 'It was great doing business with you.',
+  termLabel: 'Terms & Conditions',
+  term: 'No return deal, Thank you.',
+}
